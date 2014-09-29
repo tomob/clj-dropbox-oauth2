4f3f31b521acffaa6b76e5b7180aafec2d76c745
tomob <yubizume@gmail.com>
2014-09-29 23:19:59 +0200

    Extracted code from another project.

--- README.md
+++ README.md
@@ -3,8 +3,8 @@
 clj-dropbox-oauth2 provides clojure bindings to the Dropbox API. It
 uses Oauth2 as authorization/authentication mechanism. However, it
 does not provide any means to authenticate clients. Use
-[friend-oauth2][https://github.com/ddellacosta/friend-oauth2] or
-[clj-oauth2][https://github.com/sudharsh/clj-oauth2] to authenticate
+[friend-oauth2](https://github.com/ddellacosta/friend-oauth2) or
+[clj-oauth2](https://github.com/sudharsh/clj-oauth2) to authenticate
 users with Dropbox
 
 The library depends upon `clj-oauth'.
@@ -12,12 +12,11 @@ The library depends upon `clj-oauth'.
 ## Usage
 
 ```clojure
-(require '[clj-dropox-oauth2 :as dropbox])
+(require '[clj-dropox-oauth2.dropbox :as dropbox])
 
 ;; request user's access token by other means...
 
-(def found-files (dropbox/search access-token :dropbox "/path/to/directory"
-"*.pdf"))
+(def found-files (dropbox/search access-token :dropbox "/path/to/directory" "*.pdf"))
 
 (def file (dropbox/file access-token "/path/to/my/file.txt"))
 ```
---
+++ src/clj_dropbox_oauth2/dropbox.clj
@@ -0,0 +1,63 @@
+(ns clj-dropbox-oauth2.dropbox
+  (:require [clj-oauth2.client :as oauth2]
+            [cheshire.core :as json]))
+
+(defn- credentials [access-token]
+  {:access-token access-token :token-type "bearer"})
+
+(defn- parse-body [raw]
+  (json/parse-string (:body raw) true))
+
+(defn- do-request
+  ([client access-token url]
+     (do-request client access-token url {}))
+  ([client access-token url params]
+     (let [credentials (credentials access-token)]
+       (apply client [url (merge params {:oauth2 credentials})]))))
+
+(defn search
+  "Searches for a given query."
+  ([access-token query] (search access-token :dropbox "" query))
+  ([access-token root path query]
+     (let [url (format "https://api.dropbox.com/1/search/%s/%s"
+                       (name root)
+                       path)
+           q {:query query}]
+       (parse-body (do-request oauth2/post access-token url {:form-params q})))))
+
+(defn metadata
+  "Retrieves file/folder metadata"
+  [access-token path]
+  (let [url (format "https://api.dropbox.com/1/metadata/auto/%s" path)]
+    (parse-body (do-request oauth2/get access-token url))))
+
+(defn files
+  "Downloads a file"
+  [access-token path]
+  (let [url (format "https://api-content.dropbox.com/1/files/auto/%s" path)]
+    (:body (do-request oauth2/get access-token url))))
+
+(defn account-info
+  "Retrieves information about the user's account"
+  [access-token]
+  (parse-body (do-request oauth2/get access-token
+                          "https://api.dropbox.com/1/account/info")))
+
+
+(defn latest-cursor
+  "Retrieves latest cursor"
+  [access-token]
+  (:cursor (parse-body (do-request oauth2/post access-token
+                                   "https://api.dropbox.com/1/delta/latest_cursor"))))
+
+(defn delta
+  "Retrieves changes since the cursor"
+  [access-token cursor]
+  (parse-body (do-request oauth2/post access-token "https://api.dropbox.com/1/delta"
+                          {:form-params {:cursor cursor}})))
+
+(defn revisions
+  "Retrieves revision history for a file"
+  [access-token path]
+  (let [url (format "https://api.dropbox.com/1/revisions/auto/%s" path)]
+    (parse-body (do-request oauth2/get access-token url))))
